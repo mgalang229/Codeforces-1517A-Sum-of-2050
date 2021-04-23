f6e6e4c9f62a2df228dd224afb5dc3680fe15992
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-04-23 23:58:55 +0800

    Create sol.cpp

---
+++ sol.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int tt;
+	cin >> tt;
+	while (tt--) {
+		long long n;
+		cin >> n;
+		auto PrintAns = [&](long long x) {
+			long long sum = 0;
+			while (x > 0) {
+				sum += (x % 10);
+				x /= 10;
+			}
+			return sum;
+		};
+		// check if 'n' is divisible by 2050
+		// if yes, get the sum of the digits of the quotient of 'n' and 2050
+		// otherwise, print -1
+		cout << (n % 2050 == 0 ? PrintAns(n / 2050) : -1) << '\n';
+	}
+	return 0;
+}
